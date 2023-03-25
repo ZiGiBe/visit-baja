fd62519547116c62afc4b9d1fd69900bd7b0f1c7
Zsolt Ginál <GinalZsolt@turr.hu>
2023-03-25 17:21:27 +0100

    footer

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -3,25 +3,38 @@
 </script>
 
 <style>
-
+footer{
+    background-color: var(--kek6);
+    color: white;
+    text-shadow: 2px 2px var(--kek2);
+    box-shadow: 0 0 10px var(--kek1);
+}
+a{
+    color:white;
+}
+p{
+    margin:0;
+}
 </style>
 
 <!-- Footer -->
-<div class="d-flex flex-wrap">
-    <h4>Elérhetőségeink</h4>
-    <p class="fw-Bold">
-        Bajai Turisztikai Nonprofit Kft
-    </p>
-    <p class="fw-medium">
-        6500 Baja, Szentháromság tér 11
-    </p>
-    <p class="fw-medium">
-        Telefon:  06 79 420 792
-    </p>
-    <p class="fw-medium">
-        E-mail
-    </p>
-    <a href="/impresszum" target="_blank" >
-        Impresszum
-    </a>
-</div>
+<footer class="d-flex justify-content-around container-fluid">
+    <h3 class="my-auto mx-3">Elérhetőségeink</h3>
+    <div class="py-3">
+        <p class="fw-Bold">
+            Bajai Turisztikai Nonprofit Kft
+        </p>
+        <p class="fw-medium">
+            6500 Baja, Szentháromság tér 11
+        </p>
+        <p class="fw-medium">
+            Telefon:  06 79 420 792
+        </p>
+        <p class="fw-medium">
+            E-mail
+        </p>
+        <a href="/impresszum" target="_blank" >
+            Impresszum
+        </a>
+    </div>
+</footer>
