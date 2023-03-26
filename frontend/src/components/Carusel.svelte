49d6df86a1b14e1ebda57905beddab12c9c76dbd
Zsolt Ginál <GinalZsolt@turr.hu>
2023-03-26 18:03:30 +0200

    carusel

---
+++ frontend/src/components/Carusel.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { Swiper, SwiperSlide } from 'svelte-swiper';
+  const options = {
+    slidesPerView: 3,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    spaceBetween: 30,
+    freeMode: true,
+  };
+  import ProgramCard from './ProgramCard.svelte';
+</script>
+<div class="my-3"> 
+
+  <Swiper {options}>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+    <SwiperSlide><ProgramCard/></SwiperSlide>
+  </Swiper>
+  
+</div>
+
+<style>
+
+</style>
--- frontend/src/views/Index.svelte
+++ frontend/src/views/Index.svelte
@@ -4,14 +4,16 @@
     import Wallapper from "../components/Wallapper.svelte";
     import ProgramCard from "../components/ProgramCard.svelte";
     import InterestCard from "../components/InterestCard.svelte";
+    import Carusel from "../components/Carusel.svelte";
 </script>
 
 <style>
 
 </style>
 
+<Menu/>
 <main>
-    <Menu/>
     <Wallapper/>
+    <Carusel/>
     <Footer/>
 </main>
