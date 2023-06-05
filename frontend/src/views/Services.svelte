6f6b40716f44c26c9f4b1ecaac41d6e68277e8bd
beretkag <beretkag@gmail.com>
2023-06-05 16:31:29 +0200

    Services header

--- frontend/src/views/Services.svelte
+++ frontend/src/views/Services.svelte
@@ -4,21 +4,65 @@
 </script>
 
 <style>
+  .fade-effect{
+    color: white;
+    background: rgb(0,147,200);
+    opacity: 0.7;
+    height: inherit;
+    overflow: hidden;
+  }
+
+  .fade-effect h1{
+    padding-top: 2vh;
+  }
+
+  header{
+    background-image: url('hatter.jpg');
+    background-size: cover;
+    background-position: center center;
+    height: 17vh;
+    margin-bottom: 6vh;
+  }
+
+
+  .wave{
+        min-width: 100%;
+        height: 15vh;
+        z-index: 2;
+        position: relative;
+        top: -9vh;
+    }
+
+    @media screen and (max-width: 768px) {
+        .wave{
+           object-fit: cover;
+        }
+    }
 
 </style>
-<Menu/>
+
 
 <div>
+  <Menu/>
   <header>
-    <h1>
-      {#if isRestaurant==true}
-      Éttermek
-      {:else}
-      Szálláshelyek
-      {/if}
-    </h1>  
+    <div class="fade-effect">
+      <h1 class="offset-3 col-6">
+        {#if isRestaurant==true}
+        Éttermek
+        {:else}
+        Szálláshelyek
+        {/if}
+      </h1>
+    </div>
+    <img src="/wave.png" alt="wave" class="wave">
   </header>
-  <main>
+  <main class="col-12 offset-md-2 col-md-10 offset-lg-3 col-lg-6 p-3 d-flex flex-column">
+    <div>
+      Tartalom
+    </div>
+    <div>
+      Második tartalom
+    </div>
     
   </main>
 </div>
