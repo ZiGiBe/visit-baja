d77e91ef511136c269e239be4b3f3165a805d7da
beretkag <beretkag@gmail.com>
2023-06-12 12:36:15 +0200

    services fix_1

--- frontend/src/components/ServiceCard.svelte
+++ frontend/src/components/ServiceCard.svelte
@@ -11,11 +11,11 @@
 </script>
 
 <div class="serviceCard row">
-    <div class="col-12 col-sm-12 col-md-12 col-lg-4 me-0 p-0 z-2">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-5 me-0 p-0 z-2">
         <img src="/kep_normal.jpg" alt="thumbnail" class="thumbnail">
     </div>
-    <div class="col-12 col-sm-12 col-md-12 col-lg-8 ms-0 row ps-0 z-1 cover">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-9 ps-0">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-7 ms-0 d-flex justify-content-between flex-column ps-0 z-1 cover">
+        <div class="col-12 ps-0">
             <div class="serviceNameWideScreen" data-aos="slide-right" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-once="true">
                 Bajai Csodálatos messzire néző Türr kilátó
             </div>
@@ -23,18 +23,15 @@
                 Bajai Csodálatos messzire néző Türr kilátó
             </div>
         </div>
-        <div class="col-12 col-sm-12 col-md-12 col-lg-3 p-0">
-            <a href="/" class="buttonWideScreen btn">
-                Érdekel
-            </a>
-        </div>
-        <div class="col-12 p-3 mt-3 mt-sm-3 mt-md-3 mt-lg-0">
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem minus quo earum nostrum laborum doloremque alias soluta voluptas, ab accusamus modi molestiae reprehenderit facere. Excepturi molestias perferendis iure illum ipsam?
-            </p>
+        <div class="col-12 p-3 mt-3 mt-sm-3 mt-md-3 mt-lg-0 text-end">
+            <ul class="list-unstyled contacts">
+                <li>Baja Bácska tér 1 <img src="/map_pictogram.png" alt="map" class="icon"></li>
+                <li>30/362 7236 <img src="/phone_pictogram.png" alt="phone" class="icon"></li>
+                <li>visitbaja@gmail.com <img src="/email_pictogram.png" alt="email" class="icon"></li>
+            </ul>
         </div>
         <div class="text-center">
-            <a href="/" class="buttonThinScreen btn">
+            <a href="/" class="button btn">
                 Érdekel
             </a>
         </div>
@@ -42,9 +39,18 @@
 </div>
 
 <style>
+    .contacts{
+        font-size: large;
+    }
+
+    .icon{
+        height: 22px;
+    }
+
     .thumbnail{
         width: 100%;
-        height: 30vh;
+        min-height: 30vh;
+        height: 100%;
         object-fit: cover;
         box-shadow: -2px 5px 20px gray;
         border-radius: 5px;
@@ -54,7 +60,9 @@
         box-shadow: 2px 2px 30px rgba(0, 0, 0, 0.363);
         padding: 20px;
         border-radius: 10px;
+        height:100%;
     }
+
 
     .serviceNameWideScreen{
         padding: 10px;
@@ -83,15 +91,7 @@
     }
 
 
-    .buttonWideScreen{
-        background-color: var(--kek5);
-        color: white;
-        border-radius: 50px;
-        padding: 10px 15px;
-        position: absolute;
-    }
-
-    .buttonThinScreen{
+    .button{
         background-color: var(--kek5);
         color: white;
         border-radius: 50px;
@@ -101,11 +101,11 @@
 
 
     @media screen and (max-width: 992px) {
-        .buttonWideScreen, .serviceNameWideScreen{
+        .serviceNameWideScreen{
             display: none;
         }
 
-        .buttonThinScreen, .serviceNameThinScreen{
+        .serviceNameThinScreen{
             display: unset;
         }
 
@@ -115,13 +115,6 @@
     }
 
     @media screen and (min-width: 992px) {
-        .buttonWideScreen{
-            display: unset;
-        }
-
-        .buttonThinScreen{
-            display: none;
-        }
         .cover{
             overflow-x: hidden;
         }
