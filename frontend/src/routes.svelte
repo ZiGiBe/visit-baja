02d15998e8bf7f20a6a925a5594c5289112b251a
beretkag <beretkag@gmail.com>
2023-06-14 10:32:17 +0200

    he?

--- frontend/src/routes.svelte
+++ frontend/src/routes.svelte
@@ -4,12 +4,15 @@
     import Services from './views/Services.svelte';
     import { Route } from 'tinro';
     import Login from './views/Login.svelte';
+    import SubRoute from './views/SubRoute.svelte';
 </script>
 
 
 
 <Route path="/"><Index/></Route>
-<Route path="/ettermek"><Services isRestaurant={true} /></Route>
-<Route path="/szallasok"><Services isRestaurant={false} /></Route>
+<Route path="/sub" redirect="/sub/ettermek">
+    <Route path="/ettermek" fallback><Services isRestaurant={true} /></Route>
+    <Route path="/szallasok"><Services isRestaurant={false} /></Route>
+</Route>
 <Route path="/admin"><Admin/></Route>
 <Route path="/admin/login"><Login/></Route>
---
+++ frontend/src/views/SubRoute.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Services from "./Services.svelte";
+    import {meta} from 'tinro';
+
+</script>
+
