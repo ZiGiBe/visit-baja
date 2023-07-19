666c4b9c097cd35ebb34180eb46441a1d010a4ab
Zsolt Ginál <GinalZsolt@turr.hu>
2023-07-19 09:34:07 +0200

    impresszum

---
+++ frontend/src/views/Impresszum.svelte
@@ -0,0 +1,40 @@
+<main>
+
+    <h1>Impresszum</h1>
+
+    <p>
+        Cégnév: Bajai Turisztikai Nonprofit Kft.
+    </p>
+    <p>
+        Cím: 6500 Baja, Szentháromság tér 11
+    </p>
+    <p>
+        Ügyvezető: Schäfer Andrea
+    </p>
+    <p>
+        Telefon:  <a href="tel:0679420792">06 79 420 792</a>
+    </p>
+    <p>
+        E-mail: baja@tourinform.hu
+    </p>
+    <p>
+        A céget bejegyző hatóság: 
+    </p>
+    <p>
+        Cégjegyzékszám: 03 09 125234
+    </p>
+    <p>
+        Adószám:
+    </p>
+    <p>
+        Tárhely szolgáltató: <a href="www.elin.hu">elin.hu</a>
+    </p>
+    <p>
+        A portál és a megjelenített információk tulajdonosa:
+        Bajai Turisztikai Nonprofit Kft.
+    </p>
+    <p>
+        Az oldal szerkesztője: <a href="https://github.com/ZiGiBe">ZiGiBe csapat</a>
+    </p>
+
+</main>
