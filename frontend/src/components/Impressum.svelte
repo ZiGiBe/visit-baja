c084ebaba73bd0a0160bc6f54a5b3e1e7248e885
beretkag <beretkag@gmail.com>
2023-07-19 09:45:04 +0200

    email

--- frontend/src/components/Impressum.svelte
+++ frontend/src/components/Impressum.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main class="text-center">
-    
+
     <p>
         Cégnév: Bajai Turisztikai Nonprofit Kft.
     </p>
@@ -12,35 +12,32 @@
         Cím: 6500 Baja, Szentháromság tér 11
     </p>
     <p>
-        Ügyvezető: 
+        Ügyvezető: Schäfer Andrea
     </p>
     <p>
-        Telefon:  06 79 420 792
+        Telefon:  <a href="tel:0679420792">06 79 420 792</a>
     </p>
     <p>
-        E-mail: 
-    </p>
-    <p>
-        Internet: http://www.bajaiturizmus.hu    
+        E-mail: <a href="mailto:baja@tourinform.hu">baja@tourinform.hu</a>
     </p>
     <p>
         A céget bejegyző hatóság: 
     </p>
     <p>
-        Cégjegyzékszám: 03 09 125234
+        Cégjegyzékszám:
     </p>
     <p>
         Adószám:
     </p>
     <p>
-        Tárhely szolgáltató: 
+        Tárhely szolgáltató: <a href="www.elin.hu">elin.hu</a>
     </p>
     <p>
         A portál és a megjelenített információk tulajdonosa:
         Bajai Turisztikai Nonprofit Kft.
     </p>
     <p>
-        Az oldal szerkesztője: ZiGiBe
+        Az oldal szerkesztője: <a href="https://github.com/ZiGiBe">ZiGiBe csapat</a>
     </p>
 
 </main>
