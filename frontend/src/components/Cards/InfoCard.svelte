d24dea4f1130b946fbf3d15f685204f583141b86
Zsolt Ginál <GinalZsolt@turr.hu>
2023-08-28 08:52:52 +0200

    hotfix

--- frontend/src/components/Cards/InfoCard.svelte
+++ frontend/src/components/Cards/InfoCard.svelte
@@ -77,7 +77,7 @@
     <div class="card m-3 p-3 d-flex flex-column align-items-center mx-auto">
         <h3 class="text-center mt-3 w-100">{InfoData.title}</h3>
         <img src="{BackendUrl}media/{InfoData.image}" class="card-img-top rounded mt-3" alt="kép">
-        <a href="/latnivalok/{InfoData.href}" class="btn btn-primary">
+        <a href="{InfoData.href}" target="_blank" class="btn btn-primary">
             Tovább
         </a>
       </div>
